43b2b747644acc987e5e2092272550c41260e05e
Tomasz Janczuk <tomasz@janczuk.org>
2013-03-07 00:56:19 -0800

    support for npm test

--- test/build.bat
+++ test/build.bat
@@ -1,1 +1,2 @@
 csc /target:library /debug /out:"%~dp0\Owin.Tests.dll" "%~dp0\tests.cs" 
+if %ERRORLEVEL% NEQ 0 echo Test build failed & exit /b -1
---
+++ test/test.bat
@@ -0,0 +1,5 @@
+call %~dp0\build.bat
+if %ERRORLEVEL% NEQ 0 exit /b -1;
+pushd %~dp0\..
+call mocha
+popd
