cafee423953d57e053fce40ffeeecd53ec91b965
Dirk Baeumer <dirk.baeumer@gmail.com>
2013-03-24 20:45:28 +0100

    Enable calling JS functions from C# after the Task has been completed

--- test/103_net2node.js
+++ test/103_net2node.js
@@ -1,3 +1,19 @@
+/**
+ * Portions Copyright (c) Microsoft Corporation. All rights reserved. 
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  http://www.apache.org/licenses/LICENSE-2.0  
+ *
+ * THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS
+ * OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION 
+ * ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR 
+ * PURPOSE, MERCHANTABLITY OR NON-INFRINGEMENT. 
+ *
+ * See the Apache Version 2.0 License for specific language governing 
+ * permissions and limitations under the License.
+ */
 var edge = require('../lib/edge.js'), assert = require('assert');
 
 var edgeTestDll = __dirname + '\\Edge.Tests.dll';
@@ -118,6 +134,53 @@ describe('async call from .net to node.js', function () {
 			assert.equal(typeof result, 'string');
 			assert.ok(result.indexOf('Sample Node.js exception') > 0);
 			done();
+		});
+	});				
+});
+
+describe('delayed call from node.js to .net', function () {
+
+	it('succeeds for one callback after Task', function (done) {
+		var expected = [
+			'InvokeBackAfterCLRCallHasFinished#EnteredCLR',
+			'InvokeBackAfterCLRCallHasFinished#LeftCLR',
+			'InvokeBackAfterCLRCallHasFinished#ReturnedToNode',
+			'InvokeBackAfterCLRCallHasFinished#CallingCallbackFromDelayedTask',
+		]
+
+		var func = edge.func({
+			assemblyFile: edgeTestDll,
+			methodName: 'InvokeBackAfterCLRCallHasFinished'
+		});
+
+		var ensureNodejsFuncIsCollected = edge.func({
+			assemblyFile: edgeTestDll,
+			methodName: 'EnsureNodejsFuncIsCollected'
+		});
+
+		var trace = [];
+		var payload = {
+			eventCallback: function(result, callback) {
+				trace.push(result);
+				callback();
+				assert.ok(expected.length === trace.length);
+				for (var i = 0; i < expected.length; i++) {
+					assert.ok(expected[i] === trace[i]);
+				}
+				ensureNodejsFuncIsCollected(null, function(error, result) {
+					assert.ifError(error);
+					assert.ok(result);
+					done();
+				});
+			}
+		}
+
+		func(payload, function (error, result) {
+			assert.ifError(error);
+			result.forEach(function(entry) {
+				trace.push(entry);
+			});
+			trace.push('InvokeBackAfterCLRCallHasFinished#ReturnedToNode');
 		});
 	});
 });
