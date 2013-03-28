c15c8630c8f5bae6fc1d63b65a36b8849f1fb391
Tomasz Janczuk <tomasz@janczuk.org>
2013-03-28 16:34:01 -0700

    sample for yielding control over process lifetime to CLR

---
+++ samples/111_clr_listener.js
@@ -0,0 +1,62 @@
+// Overview of edge.js: http://tjanczuk.github.com/edge
+
+var edge = require('../lib/edge');
+
+var createController = edge.func(function () {/*
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class Startup
+    {
+        static TaskCompletionSource<object> tcs;
+
+        public async Task<object> Invoke(object input)
+        {
+            return new {
+                yieldControl = (Func<object,Task<object>>)((i) => {
+                    Startup.AcquireControl();
+                    return Startup.tcs.Task;
+                }),
+                regainControl = (Func<object,Task<object>>)(async (i) => {
+                    Startup.ReleaseControl();
+                    return null;
+                })
+            };
+        }
+
+        static void AcquireControl()
+        {
+            // single threaded; always called on V8 thread
+
+            if (tcs != null)
+            {
+                throw new InvalidOperationException("CLR already controls the lifetime of the process.");
+            }
+
+            tcs = new TaskCompletionSource<object>();
+        }
+
+        public static void ReleaseControl()
+        {
+            // multi-threaded; can be called from V8 or one of many CLR threads
+
+            TaskCompletionSource<object> tmp = Interlocked.Exchange(ref tcs, null);
+            if (tmp != null)
+            {
+                tmp.SetResult(null);
+            }
+        }
+    }
+*/});
+
+// yield control over process lifetime to CLR
+var controller = createController(null, true);
+controller.yieldControl();
+console.log('Control over process lifetime yielded to CLR, the process will not exit...');
+
+// at this point, the process will not exit until one of the following happens:
+// - node.js calls controller.regainControl(), or
+// - CLR calls Startup.ReleaseControl()
+
+// controller.regainControl();
