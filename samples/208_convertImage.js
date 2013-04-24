07e6880a7e27c537b9eb5efe1627d862300bc954
Tomasz Janczuk <tomasz@janczuk.org>
2013-04-23 22:10:07 -0700

    sample for image type conversion

---
+++ samples/208_convertImage.js
@@ -0,0 +1,48 @@
+// Overview of edge.js: http://tjanczuk.github.com/edge
+
+var edge = require('../lib/edge');
+
+var convertImage = edge.func(function() {/*
+    //#r "System.Drawing.dll"
+
+    using System;
+    using System.Threading.Tasks;
+    using System.Collections.Generic;
+    using System.Drawing;
+    using System.Drawing.Imaging;
+
+    class Startup
+    {
+        static IDictionary<string, ImageFormat> formats = new Dictionary<string, ImageFormat> 
+        {
+            { "jpg", ImageFormat.Jpeg },
+            { "bmp", ImageFormat.Bmp },
+            { "gif", ImageFormat.Gif },
+            { "tiff", ImageFormat.Tiff },
+            { "png", ImageFormat.Png }
+        };
+
+        public async Task<object> Invoke(IDictionary<string,object> input)
+        {
+            await Task.Run(async () => {
+                using (Image image = Image.FromFile((string)input["source"]))
+                {
+                    image.Save((string)input["destination"], formats[(string)input["toType"]]);
+                }
+            });
+
+            return null;
+        }
+    }
+*/});
+
+var params = { 
+    source: '.\\edge.png', 
+    destination: '.\\edge.jpg',
+    toType: 'jpg'
+};
+
+convertImage(params, function (error) {
+    if (error) throw error;
+    console.log('The edge.png has been asynchronously converted to edge.jpg.');
+});
