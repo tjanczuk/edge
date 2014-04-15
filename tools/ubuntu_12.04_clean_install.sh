98365b2142b06b8540b579be86959ea928a0955f
Tomasz Janczuk <tomasz@janczuk.org>
2014-04-15 14:26:13 -0700

    mono: update ubuntu install script

--- tools/ubuntu_12.04_clean_install.sh
+++ tools/ubuntu_12.04_clean_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -e
-sudo -u ${USERNAME} mkdir ~/tmp
+sudo -u ${USER} mkdir ~/tmp
 
 # install prerequisities
 
@@ -9,11 +9,11 @@ apt-get install curl g++ pkg-config
 
 # download and build Node.js
 
-sudo -u ${USERNAME} curl https://codeload.github.com/joyent/node/tar.gz/v0.10.26 > node.v0.10.26.tar.gz
-sudo -u ${USERNAME} tar -xvf node.v0.10.26.tar.gz
+sudo -u ${USER} curl https://codeload.github.com/joyent/node/tar.gz/v0.10.26 > node.v0.10.26.tar.gz
+sudo -u ${USER} tar -xvf node.v0.10.26.tar.gz
 cd node-0.10.26/
-sudo -u ${USERNAME} bash -c './configure'
-sudo -u ${USERNAME} make
+sudo -u ${USER} bash -c './configure'
+sudo -u ${USER} make
 make install
 cd ..
 
@@ -24,12 +24,12 @@ npm install mocha -g
 
 # download and build Mono x64
 
-sudo -u ${USERNAME} curl http://download.mono-project.com/sources/mono/mono-3.4.0.tar.bz2 > mono-3.4.0.tar.bz2
-sudo -u ${USERNAME} tar -xvf mono-3.4.0.tar.bz2
-sudo -u ${USERNAME} curl https://raw.githubusercontent.com/tjanczuk/edge/mono/tools/Microsoft.Portable.Common.targets > ./mono-3.4.0/mcs/tools/xbuild/targets/Microsoft.Portable.Common.targets
+sudo -u ${USER} curl http://download.mono-project.com/sources/mono/mono-3.4.0.tar.bz2 > mono-3.4.0.tar.bz2
+sudo -u ${USER} tar -xvf mono-3.4.0.tar.bz2
+sudo -u ${USER} curl https://raw.githubusercontent.com/tjanczuk/edge/mono/tools/Microsoft.Portable.Common.targets > ./mono-3.4.0/mcs/tools/xbuild/targets/Microsoft.Portable.Common.targets
 cd mono-3.4.0
-sudo -u ${USERNAME} bash -c './configure --prefix=/usr/local --with-glib=embedded --enable-nls=no'
-sudo -u ${USERNAME} make
+sudo -u ${USER} bash -c './configure --prefix=/usr/local --with-glib=embedded --enable-nls=no'
+sudo -u ${USER} make
 make install
 ln -s /lib/x86_64-linux-gnu/libc.so.6 /lib/x86_64-linux-gnu/libc.so
 ldconfig
@@ -37,8 +37,8 @@ cd ..
 
 # download and build Edge.js
 
-sudo -u ${USERNAME} curl https://codeload.github.com/tjanczuk/edge/zip/mono > edge.js.zip
-sudo -u ${USERNAME} unzip edge.js.zip 
+sudo -u ${USER} curl https://codeload.github.com/tjanczuk/edge/zip/mono > edge.js.zip
+sudo -u ${USER} unzip edge.js.zip 
 cd edge-mono/
-sudo -u ${USERNAME} npm install
-sudo -u ${USERNAME} npm test
+sudo -u ${USER} npm install
+sudo -u ${USER} npm test
