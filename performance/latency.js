f4384fbcd1798f8acb3d31f960858174586c3408
Tomasz Janczuk <tomasz@janczuk.org>
2014-04-17 20:43:16 -0700

    simple latency test

---
+++ performance/latency.js
@@ -0,0 +1,60 @@
+function help() {
+    console.log('Usage: node latency.js i|x <N>');
+    console.log('   i - in-process loop of N iterations');
+    console.log('   x - child process loop of N iterations');
+    console.log('e.g. node latency.js i 1000');
+    process.exit(1);    
+}
+
+if (process.argv.length !== 4)
+    help();
+
+if (isNaN(process.argv[3]))
+    help();
+
+var N = +process.argv[3];
+
+if (N < 1)
+    help();
+
+if (process.argv[2] === 'i') {
+    var func = require('../lib/edge').func(function () {/*
+        async (input) => {
+            return ".NET welcomes " + input.ToString();
+        }
+    */});
+    var M = N;
+    var start = Date.now();
+    one_i();
+    function one_i() {
+        func('Node.js', function (error, result) {
+            if (error) throw error;
+            if (--M <= 0) {
+                var delta = Date.now() - start;
+                console.log(delta, delta / N);
+            }
+            else 
+                setImmediate(one_i);
+        });
+    }
+}
+else if (process.argv[2] === 'x') {
+    var child_process = require('child_process');
+    var cmd = [process.argv[0], process.argv[1], 'i', '1'].join(' ');
+    var M = N;
+    var start = Date.now();
+    one_x();
+    function one_x() {
+        child_process.exec(cmd, function (error) {
+            if (error) throw error;
+            if (--M <= 0) {
+                var delta = Date.now() - start;
+                console.log(delta, delta / N);
+            }
+            else 
+                setImmediate(one_x);
+        });
+    }
+}
+else
+    help();
