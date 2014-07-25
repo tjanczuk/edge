1c51bc8aedd9cac91e8b5356ddcd0ef86baa9d23
Sebastian.Just <sebastian.just@sungard.com>
2014-07-25 00:35:32 -0400

    Adding handling for other "primitive" types as Int etc. Also nested types are supported Collection support still pending

--- src/dotnet/utils.cpp
+++ src/dotnet/utils.cpp
@@ -45,6 +45,87 @@ System::String^ exceptionV82stringCLR(Handle<v8::Value> exception)
     return gcnew System::String(stringV82CLR(Handle<v8::String>::Cast(exception)));
 }
 
+bool IsGenericList(System::Type^ type)
+{
+    if (type == nullptr) {
+		return false;
+    }
+	array<System::Type^>^ interfaces = type->GetInterfaces();
+	for(int i=0; i < interfaces->Length; i++)
+	{
+		System::Type^ x = interfaces[i];
+		
+		if (System::Collections::ICollection::typeid == x) {
+			return true;
+		}
+
+
+    }
+    return false;
+}
+
+Handle<Value> mapItem(System::Object^ value)
+{
+	if (value->GetType() == System::String::typeid)
+	{
+		return stringCLR2V8((System::String^)value);
+	}
+	else if (value->GetType() == System::Guid::typeid)
+	{
+		return stringCLR2V8(((System::Guid^)value)->ToString());
+	}
+	else if (value->GetType() == System::Int32::typeid)
+	{
+		return v8::Integer::New((System::Int32)value);
+	}
+	else if (value->GetType() == System::Byte::typeid)
+	{
+		return v8::Integer::New((System::Byte)value);
+	}
+	else if (value->GetType() == System::Int16::typeid)
+	{
+		return v8::Integer::New((System::Int16)value);
+	}
+	else if (value->GetType() == System::Single::typeid)
+	{
+		return v8::Number::New((System::Single)value);
+	}
+	else if (value->GetType() == System::Double::typeid)
+	{
+		return v8::Number::New((System::Double)value);
+	}
+	else if (value->GetType() == System::Boolean::typeid)
+	{
+		return v8::BooleanObject::New((System::Boolean)value);
+	}
+	else if (System::Exception::typeid->IsAssignableFrom(value->GetType()))
+	{
+		return exceptionCLR2stringV8((System::Exception^)value);
+	}/*
+	else if (IsGenericList(value->GetType()))
+	{
+		System::Collections::Generic::IList sourceValues;
+		sourceValues = (System::Collections::Generic::IList<System::Object^>^)value;
+		Local< Array > targetValues = Array::New(sourceValues->Count);
+		
+		for(int j = 0; j < sourceValues->Count; j++)
+		{
+			System::Object^ sourceValue = sourceValues[j];
+			Handle<Value> targetValue = mapItem(sourceValue);
+			
+			targetValues->Set(j, targetValue);
+		}
+			
+		return targetValues;
+
+	}*/	
+	else
+	{
+		// At least show the type
+		return stringCLR2V8(value->GetType()->FullName);
+	}
+}
+
 Handle<Value> exceptionCLR2stringV8(System::Exception^ exception)
 {
     HandleScope scope;
@@ -71,25 +152,16 @@ Handle<Value> exceptionCLR2stringV8(System::Exception^ exception)
 		//Walk over all attributes and copy them
 		array<PropertyInfo^>^ properties = exception->GetType()->GetProperties();
 		
-		for(int i = 0; i < properties->Length; i++){
-
+		for(int i = 0; i < properties->Length; i++)
+		{
 			PropertyInfo^ pi = properties[i];
 			Handle<v8::String> propertyName = stringCLR2V8(pi->Name);
 				
 			System::Object^ value = pi->GetValue(exception);
-			if (value != nullptr){
-				System::String^ propertyType = value->GetType()->FullName;
-				Handle<v8::String> propertyValue;
-				//There should be a better approach:
-				if (propertyType->Equals("System.String")) {
-					propertyValue = stringCLR2V8((System::String^)value);
-				} else {
-					propertyValue = stringCLR2V8(propertyType);
-				}
-				
-				obj->Set(propertyName, propertyValue);
+			if (value != nullptr)
+			{
+				obj->Set(propertyName, mapItem(value));
 			}
-			//obj->Set(String::NewSymbol((char*)(void*)System::Runtime::InteropServices::Marshal::StringToHGlobalAnsi(pi->Name)), Message);
 		}
 		//Record the whole toString for those who are interessted
 
