6d5b590d9996996409191e1a2287696f90fab7a5
Luke Stratman <lstratman@gmail.com>
2015-07-16 10:55:36 -0400

    Testing script property accounts for CoreCLR

--- tools/test.js
+++ tools/test.js
@@ -7,7 +7,24 @@ var spawn = require('child_process').spawn
 	, winBuild = []
 	, monoBuild = ['-sdk:4.5']
 	, mocha = path.resolve(__dirname, '../node_modules/mocha/bin/mocha')
+	, fs = require('fs')
 	;
+
+if(process.env.EDGE_USE_CORECLR && process.platform !== 'win32') {
+	var dnxPath = path.dirname(whereis('dnx'));
+
+	monoBuild = monoBuild.concat([
+		'-nostdlib',
+		'-noconfig',
+		'-r:' + path.join(dnxPath, 'System.Collections.dll'),
+		'-r:' + path.join(dnxPath, 'mscorlib.dll'),
+		'-r:' + path.join(dnxPath, 'System.Dynamic.Runtime.dll'),
+		'-r:' + path.join(dnxPath, 'Microsoft.CSharp.dll'),
+		'-r:' + path.join(dnxPath, 'System.ObjectModel.dll'),
+		'-r:' + path.join(dnxPath, 'System.Runtime.dll'),
+		'-r:' + path.join(dnxPath, 'System.Linq.Expressions.dll')
+	]);
+}
 
 if(process.platform === 'win32') {
 	spawn('csc', winBuild.concat(buildParameters), { stdio: 'inherit' })
@@ -22,4 +39,20 @@ function runOnSuccess(code, signal) {
 		spawn('node', [mocha, testDir, '-R', 'spec'], { stdio: 'inherit' })
 			.on('error', function(err) { console.log(err); });
 	}
+}
+
+function whereis(filename){
+    var pathSep = process.platform === 'win32' ? ';' : ':';
+
+    var directories = process.env.PATH.split(pathSep);
+
+    for (var i = 0; i < directories.length; i++) {
+        var path = directories[i] + '/' + filename;
+
+        if (fs.existsSync(path)) {
+            return path;
+        }
+    }
+
+    return null;
 }
