b66e4b58dd508cea2e89a543e6d95d3b2cd20644
Luke Stratman <lstratman@gmail.com>
2015-07-23 14:11:49 -0400

    .NET callback function for when a Node.js function finishes executing is now static and the underlying delegate will not be disposed early.

--- src/CoreCLREmbedding/nodejsfuncinvokecontext.cs
+++ src/CoreCLREmbedding/nodejsfuncinvokecontext.cs
@@ -9,7 +9,14 @@ public class NodejsFuncInvokeContext
 {
     private NodejsFunc functionContext;
     private object payload;
+    private static IntPtr NodejsFuncCompleteCallback;
     private static NodejsFuncCompleteDelegate NodeJsFuncCompleteInstance;
+
+    static NodejsFuncInvokeContext()
+    {
+        NodeJsFuncCompleteInstance = new NodejsFuncCompleteDelegate(NodejsFuncComplete);
+        NodejsFuncCompleteCallback = Marshal.GetFunctionPointerForDelegate<NodejsFuncCompleteDelegate>(NodeJsFuncCompleteInstance);
+    }
 
     internal static CallV8FunctionDelegate CallV8Function
     {
@@ -43,7 +50,7 @@ public class NodejsFuncInvokeContext
         CoreCLREmbedding.DebugMessage("NodejsFuncInvokeContext::CallFunc (CLR) - Getting a GCHandle for this context object");
 
         CoreCLREmbedding.DebugMessage("NodejsFuncInvokeContext::CallFunc (CLR) - Invoking the unmanaged V8 function to invoke the Node.js function on the V8 thread");
-        CallV8Function(v8Payload, (int)v8PayloadType, functionContext.Context, callbackContext, Marshal.GetFunctionPointerForDelegate<NodejsFuncCompleteDelegate>(NodejsFuncComplete));
+        CallV8Function(v8Payload, (int)v8PayloadType, functionContext.Context, callbackContext, NodejsFuncCompleteCallback);
     }      
 
     public static void NodejsFuncComplete(IntPtr context, int taskStatus, IntPtr result, int resultType)
