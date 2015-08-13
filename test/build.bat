458aa963e390d90f00faeda2bd40cf6db8b37001
Luke Stratman <lstratman@gmail.com>
2015-08-13 12:23:37 -0400

    Updated the Windows unit test scripts to properly build the CoreCLR test assembly.

--- test/build.bat
+++ test/build.bat
@@ -1,2 +1,7 @@
+set SELF=%~dp0
+
 csc /target:library /debug /out:"%~dp0\Edge.Tests.dll" "%~dp0\tests.cs" 
 if %ERRORLEVEL% NEQ 0 echo Test build failed & exit /b -1
+cmd /c "cd ""%SELF%"" && dnu restore && dnu build"
+if %ERRORLEVEL% NEQ 0 echo Test build failed & exit /b -1
+copy "%SELF%bin\Debug\dnxcore50\test.dll" "%SELF%Edge.Tests.CoreClr.dll"
