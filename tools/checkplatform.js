392d1eace299f05e93d2c4f169b4805bf504b0d7
Jianchun Xu <jianxu@microsoft.com>
2016-03-01 15:15:38 -0800

    win32: try to build if no precompiled binaries
    On win32 if no precompiled binaries found, try to invoke
    ```build.bat release```.
    
    Without binaries this module cannot be used. So invoking build might be
    more useful than output a message, suppose the build script works well.

--- tools/checkplatform.js
+++ tools/checkplatform.js
@@ -1,10 +1,18 @@
+var
+  path = require('path'),
+  spawn = require('child_process').spawn;
+
 try {
 	require('../lib/edge.js');
 }
 catch (e) {
-	console.log('***************************************');
-	console.log(e);
-	console.log('***************************************');
+  if (process.platform === 'win32' && /module has not been pre-compiled/.exec(e)) {
+    spawn(path.resolve(__dirname, 'build.bat'), ['release'], { stdio: 'inherit' });
+  } else {
+    console.log('***************************************');
+    console.log(e);
+    console.log('***************************************');
+  }
 }
 
 console.log('Success: platform check for edge.js: node.js ' + process.arch + ' v' + process.versions.node);
