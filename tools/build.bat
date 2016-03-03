fb2a46771093204a19c6923559774541a5fb7b2e
Jianchun Xu <jianxu@microsoft.com>
2016-03-02 17:16:42 -0800

    make edge builds on node-chakracore
    Piggy back on "tools/build.bat" to build for the current installed node
    release when no VERSION is specified. If so save the build to subdir
    node-ENGINE-VERSION instead of VERSION.
    
    Update edge.js to check node-ENGINE-VERSION subdir before looking at known
    versionMap.
    
    Changed edge.js "new Error().stack" to "Error.captureStackTrace()" which
    works on both node-v8 and node-chakracore.
    
    Fix "whereis.js" where it was returnning "null" when target is not found.
    ```binding.gyp``` was testing ```console.log(result)``` output to be empty
    string "" when target not found.

--- tools/build.bat
+++ tools/build.bat
@@ -3,6 +3,9 @@ set SELF=%~dp0
 if "%1" equ "" (
     echo Usage: build.bat debug^|release "{version} {version}" ...
     echo e.g. build.bat release "0.8.22 0.10.0"
+    echo      or
+    echo      build.bat release
+    echo                ^(builds for the current node.exe release^)
     exit /b -1
 )
 
@@ -24,31 +27,48 @@ if "%1" neq "" (
     shift
     goto :harvestVersions
 )
-if "%VERSIONS%" equ "" set VERSIONS=0.10.0
+if "%VERSIONS%" equ "" set VERSIONS=CURRENT_INSTALL
 pushd %SELF%\..
-for %%V in (%VERSIONS%) do call :build ia32 x86 %%V 
-for %%V in (%VERSIONS%) do call :build x64 x64 %%V 
+for %%V in (%VERSIONS%) do call :build ia32 x86 %%V
+for %%V in (%VERSIONS%) do call :build x64 x64 %%V
 popd
 
 exit /b 0
 
 :build
 
-set DESTDIR=%DESTDIRROOT%\%1\%3
-if exist "%DESTDIR%\node.exe" goto gyp
+set VERSION=%3
+set CURRENT_INSTALL=
+if "%VERSION%" == "CURRENT_INSTALL" (
+    set CURRENT_INSTALL=true
+    for /f "Tokens=*" %%i in ('node -pe "process.jsEngine || 'v8'"') do set NODEENGINE=%%i
+    for /f "Tokens=*" %%i in ('node -pe "process.versions.node"') do set VERSION=%%i
+)
+
+if "%CURRENT_INSTALL%" == "" (
+    set NODERELEASE=v%VERSION%
+    set NODEEXE=%DESTDIR%\node.exe
+    set GYP=%APPDATA%\npm\node_modules\node-gyp\bin\node-gyp.js
+) else (
+    set NODERELEASE=node-%NODEENGINE%-%VERSION%
+    REM NODEEXE remains the current node.exe path unchanged. GYP uses the current node.exe install
+    set GYP=%NODEDIR%\node_modules\npm\node_modules\node-gyp\bin\node-gyp.js
+)
+
+set DESTDIR=%DESTDIRROOT%\%1\%NODERELEASE%
 if not exist "%DESTDIR%\NUL" mkdir "%DESTDIR%"
-echo Downloading node.exe %2 %3...
-node "%SELF%\download.js" %2 %3 "%DESTDIR%"
+
+if exist "%NODEEXE%" goto gyp
+echo Downloading node.exe %2 %VERSION%...
+node "%SELF%\download.js" %2 %VERSION% "%DESTDIR%"
 if %ERRORLEVEL% neq 0 (
-    echo Cannot download node.exe %2 v%3
+    echo Cannot download node.exe %2 %NODERELEASE%
     exit /b -1
 )
 
 :gyp
 
-echo Building edge.node %FLAVOR% for node.js %2 v%3
-set NODEEXE=%DESTDIR%\node.exe
-set GYP=%APPDATA%\npm\node_modules\node-gyp\bin\node-gyp.js
+echo Building edge.node %FLAVOR% for node.js %2 %NODERELEASE%
 if not exist "%GYP%" (
     echo Cannot find node-gyp at %GYP%. Make sure to install with npm install node-gyp -g
     exit /b -1
@@ -58,21 +78,21 @@ if exist ".\src\CoreCLREmbedding\bin" rmdir /s /q ".\src\CoreCLREmbedding\bin"
 
 "%NODEEXE%" "%GYP%" configure build --msvs_version=2013 -%FLAVOR%
 if %ERRORLEVEL% neq 0 (
-    echo Error building edge.node %FLAVOR% for node.js %2 v%3
+    echo Error building edge.node %FLAVOR% for node.js %2 %NODERELEASE%
     exit /b -1
 )
 
 echo %DESTDIR%
 copy /y .\build\%FLAVOR%\edge_*.node "%DESTDIR%"
 if %ERRORLEVEL% neq 0 (
-    echo Error copying edge.node %FLAVOR% for node.js %2 v%3
+    echo Error copying edge.node %FLAVOR% for node.js %2 %NODERELEASE%
     exit /b -1
 )
 
 if exist ".\build\%FLAVOR%\CoreCLREmbedding.dll" (
     copy /y .\build\%FLAVOR%\CoreCLREmbedding.dll "%DESTDIR%"
     if %ERRORLEVEL% neq 0 (
-        echo Error copying CoreCLREmbedding.dll %FLAVOR% for node.js %2 v%3
+        echo Error copying CoreCLREmbedding.dll %FLAVOR% for node.js %2 %NODERELEASE%
         exit /b -1
     )
 )
@@ -83,4 +103,4 @@ if %ERRORLEVEL% neq 0 (
     exit /b -1
 )
 
-echo Success building edge.node %FLAVOR% for node.js %2 v%3
+echo Success building edge.node %FLAVOR% for node.js %2 %NODERELEASE%
