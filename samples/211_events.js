9d5c1175ce23a936b3413155af9675f3892db5ff
Tomasz Janczuk <tomasz@janczuk.org>
2016-03-10 11:21:14 -0800

    fixups

--- samples/211_events.js
+++ samples/211_events.js
@@ -7,21 +7,25 @@ var subscribe = edge.func(function() {/*
     {
         // Create a timer with the specifed interval. 
         // Conceptually this can be any event source. 
+
         var timer = new System.Timers.Timer(input.interval);
 
         // Hook up the Elapsed event for the timer and delegate 
         // the call to a Node.js event handler. 
         // Depending on the EventArgs, the data may need to be transformed 
         // if it cannot be directly marshaled by Edge.js.
+
         timer.Elapsed += (Object source, System.Timers.ElapsedEventArgs e) => {
             ((Func<object,Task<object>>)input.event_handler)(e).Start();
         };
 
         // Start the timer
+        
         timer.Enabled = true;
 
         // Return a function that can be used by Node.js to 
         // unsubscribe from the event source.
+
         return (Func<object,Task<object>>)(async (dynamic data) => {
             timer.Enabled = false;
             return null;
